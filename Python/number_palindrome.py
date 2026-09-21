30a81efb49ca7355e0a4ea56b3cdc508a0256aae
Tanmay Janrao <tanmayjanrao.work@gmail.com>
2026-09-21 15:29:10 +0530

    update

--- Python/number_palindrome.py
+++ Python/number_palindrome.py
@@ -10,17 +10,4 @@ else:
     print("The number is not palindrome")
 
 
-# using function
-
-
-def check_palindrome(value):
-    if value == value[::-1]:
-        return "The number is a palindrome"
-    else:
-        return "The number is not a palindrome"
-
-
-number = input("Enter the number: ")
-
-print(check_palindrome(number))
 
